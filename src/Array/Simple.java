1a292b47ad70d9e5e61766bc424a99946c9089c9
Elena Kutsenko <albion.k@mail.ru>
2022-06-20 21:41:39 +0300

    Initial commit

---
+++ src/Array/Simple.java
@@ -0,0 +1,12 @@
+package Array;
+
+import java.util.Iterator;
+
+public interface Simple<E> extends Iterator <E> {
+
+    boolean add(E e);
+    void delete (int index);
+    E get (int index);
+    int size();
+    void update (int index,E e);
+}
